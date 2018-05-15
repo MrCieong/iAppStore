3148b7fd7417181a3c73704ef04a38e8f9fc0226
MrCieong <zhangjing0829@qq.com>
2018-05-15 21:52:21 +0800

    ADD: added storyboards

--- iAppStore/ViewControllers/Today/TodayViewController.swift
+++ iAppStore/ViewControllers/Today/TodayViewController.swift
@@ -14,6 +14,7 @@ class TodayViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
