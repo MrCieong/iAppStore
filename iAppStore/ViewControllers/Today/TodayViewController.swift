291bcb4867d36a4439e0e9bfabb58c4eb7105981
MrCieong <zhangjing0829@qq.com>
2018-05-15 21:51:20 +0800

    ADD: added storyboards

---
+++ iAppStore/ViewControllers/Today/TodayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TodayViewController.swift
+//  iAppStore
+//
+//  Created by 张景 on 2018/5/15.
+//  Copyright © 2018年 MrCieong. All rights reserved.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
